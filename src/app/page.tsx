448bc94605a0e4c4900cca58533ad007013caccf
Ritik Kumar <ritikkashyap013@gmail.com>
2025-09-30 18:27:23 +0530

    Fix Landing Route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { LoginForm } from "@/components/authcomponents/LoginForm";
+import Dashboard from "./dashboard/page";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-1 w-full min-h-screen items-center justify-center">
-      <LoginForm />
+    <div className=" w-full min-h-screen ">
+      <Dashboard />
     </div>
   );
 }
